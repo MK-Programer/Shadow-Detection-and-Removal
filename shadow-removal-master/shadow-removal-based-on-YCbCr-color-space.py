3499bdb8a83fee92254b6d60a13584661cc797e9
Mostafa Khaled <77085680+MK-Programer@users.noreply.github.com>
2021-06-23 12:14:45 +0200

    Add files via upload

---
+++ shadow-removal-master/shadow-removal-based-on-YCbCr-color-space.py
@@ -0,0 +1,102 @@
+import os
+import cv2
+import random
+import numpy as np
+
+# path of dataset folder
+folder=r"D:\shadow-removal-master\dataset"
+
+# select random image name
+r_img_name = random.choice(os.listdir(folder))
+# print the name of the random image 
+print(r_img_name)
+
+# concatenate the random image name with the path of the dataset folder
+r_img = folder+'\\' + r_img_name
+
+
+# read an image with shadow...
+# and it converts to BGR color space automatically
+or_img = cv2.imread(r_img)
+
+# covert the BGR image to an YCbCr image
+y_cb_cr_img = cv2.cvtColor(or_img, cv2.COLOR_BGR2YCrCb)
+cv2.imshow("YCBCR", y_cb_cr_img)
+
+# copy the image to create a binary mask later
+binary_mask = np.copy(y_cb_cr_img)
+
+# get mean(AVG.) value of the pixels in Y plane (sum / count)
+y_mean = np.mean(cv2.split(y_cb_cr_img)[0])
+
+# get standard deviation of channel in Y plane
+y_std = np.std(cv2.split(y_cb_cr_img)[0])
+
+# classify pixels as shadow and non-shadow pixels
+for i in range(y_cb_cr_img.shape[0]):
+    for j in range(y_cb_cr_img.shape[1]):
+
+        if y_cb_cr_img[i, j, 0] < y_mean - (y_std / 3):
+            # paint it white (shadow)
+            binary_mask[i, j] = [255, 255, 255]
+        else:
+            # paint it black (non-shadow)
+            binary_mask[i, j] = [0, 0, 0]
+
+# Using morphological operation
+# The misclassified pixels are
+# removed using dilation followed by erosion. (Closing)
+kernel = np.ones((3, 3), np.uint8)
+erosion = cv2.erode(binary_mask, kernel, iterations=1)
+
+# sum of pixel intensities in the lit areas
+spi_la = 0
+
+# sum of pixel intensities in the shadow
+spi_s = 0
+
+# number of pixels in the lit areas
+n_la = 0
+
+# number of pixels in the shadow
+n_s = 0
+
+# get sum of pixel intensities in the lit areas
+# and sum of pixel intensities in the shadow
+for i in range(y_cb_cr_img.shape[0]):
+    for j in range(y_cb_cr_img.shape[1]):
+        if erosion[i, j, 0] == 0 and erosion[i, j, 1] == 0 and erosion[i, j, 2] == 0:
+            spi_la = spi_la + y_cb_cr_img[i, j, 0]
+            n_la += 1
+        else:
+            spi_s = spi_s + y_cb_cr_img[i, j, 0]
+            n_s += 1
+
+# get the average pixel intensities in the lit areas
+average_ld = spi_la / n_la
+
+# get the average pixel intensities in the shadow
+average_le = spi_s / n_s
+
+# difference of the pixel intensities in the shadow and lit areas
+i_diff = average_ld - average_le
+
+# get the ratio between average shadow pixels and average lit pixels
+ratio_as_al = average_ld / average_le
+
+# color adjustment
+# added these difference
+for i in range(y_cb_cr_img.shape[0]):
+    for j in range(y_cb_cr_img.shape[1]):
+        if erosion[i, j, 0] == 255 and erosion[i, j, 1] == 255 and erosion[i, j, 2] == 255:
+
+            y_cb_cr_img[i, j] = [y_cb_cr_img[i, j, 0] + i_diff, y_cb_cr_img[i, j, 1] + ratio_as_al,
+                                 y_cb_cr_img[i, j, 2] + ratio_as_al]
+
+# convert the YCbCr image to the BGR image
+final_image = cv2.cvtColor(y_cb_cr_img, cv2.COLOR_YCR_CB2BGR)
+
+cv2.imshow("before", or_img)
+cv2.imshow("after", final_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
